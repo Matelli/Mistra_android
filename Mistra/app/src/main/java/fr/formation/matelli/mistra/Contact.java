590f737f3ec664aaec67dd3331804b72e793aad7
hdmytrow <herve.dmytrow@matelli.fr>
2015-07-08 12:29:20 +0200

    - ajout de redirection sur chaque réseau social - ajout d'une ouverture de google maps si apps présente sur le téléphone, sinon navigateur web google maps avec marqueur de position Mistra formation - ajout d'une action de "call" lorsqu'on clique sur le numéro de téléphone

--- Mistra/app/src/main/java/fr/formation/matelli/mistra/Contact.java
+++ Mistra/app/src/main/java/fr/formation/matelli/mistra/Contact.java
@@ -1,17 +1,22 @@
 package fr.formation.matelli.mistra;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.ShareActionProvider;
 import android.widget.Toast;
 
 import java.text.Normalizer;
+import java.util.Locale;
 
 import data.Formation;
 import data.Tutoriel;
@@ -19,17 +24,39 @@ import data.Tutoriel;
 
 public class Contact extends Activity {
 
-    ImageButton btnRetour;
+    private Context context;
 
-    ImageButton btnTwitter;
+    private ImageButton btnRetour;
+
+    private LinearLayout ll_adresse;
+    private LinearLayout ll_numTel;
+    private LinearLayout ll_email;
+
+
+
+    private ImageButton btnTwitter;
+    private ImageButton btnFacebook;
+    private ImageButton btnGoogle;
+    private ImageButton btnLinkedin;
+    private ImageButton btnViadeo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        context = this;
+
         setContentView(R.layout.activity_contact);
+
+        ll_adresse = (LinearLayout) findViewById(R.id.ll_adresse);
+        ll_numTel = (LinearLayout) findViewById(R.id.ll_numTel);
+        ll_email = (LinearLayout) findViewById(R.id.ll_email);
 
         btnRetour = (ImageButton) findViewById(R.id.btnRetourHome);
         btnTwitter = (ImageButton) findViewById(R.id.btn_twitter);
+        btnFacebook = (ImageButton) findViewById(R.id.btn_facebook);
+        btnGoogle = (ImageButton) findViewById(R.id.btn_google);
+        btnLinkedin = (ImageButton) findViewById(R.id.btn_linkedin);
+        btnViadeo = (ImageButton) findViewById(R.id.btn_viadeo);
 
         btnRetour.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,25 +80,119 @@ public class Contact extends Activity {
             }
         });
 
+        ll_numTel.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0182522525"));
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        ll_adresse.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                try {
+                    // Create a Uri from an intent string. Use the result to create an Intent.
+                    Uri gmmIntentUri = Uri.parse("geo:48.8661697,2.3630310999999438?q=" + Uri.encode("Mistra Formation"));
+
+                    // Create an Intent from gmmIntentUri. Set the action to ACTION_VIEW
+                    Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                    // Make the Intent explicit by setting the Google Maps package
+                    mapIntent.setPackage("com.google.android.apps.maps");
+                    if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(mapIntent);
+                        finish();
+                    } else {
+                        final String adr = new String("19 rue b&eacute;ranger, 75003 paris");
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.fr/maps/place/" + adr));
+                        startActivity(intent);
+                        finish();
+                    }
+
+
+                } catch (ActivityNotFoundException ex) {
+                    Toast.makeText(context,"Vous n'avez pas les applications nécessaires pour afficher la géolocalisation du lieu.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        ll_email.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //TODO il faut créer une vue spéciale pour une prise de contact et non pas fait appel au devis. Au pire, appeler la Composer Box
+                /*Intent i = new Intent(Contact.this, Devis.class);
+                i.putExtra("whoIam", Formation.class.toString());
+                startActivity(i);
+                finish();*/
+            }
+        });
+
+
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent facebookIntent;
+                //TODO la parti du "try" commentée ne peut etre testée via genymotion mais pire, je ne peut pas avoir l'id du profil de mistra pour le moment.
+                    /*try {
+                        context.getPackageManager().getPackageInfo("com.facebook.katana", 0); //Checks if FB is even installed.
+                        facebookIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("fb://profile/254175194653125")); //Trys to make intent with FB's URI
+                        startActivity(facebookIntent);
+                    } catch (Exception e) {*/
+                facebookIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/MistraFormation")); //catches and opens a url to the desired page
+                startActivity(facebookIntent);
+                finish();
+                //}
+
+            }
+        });
+
         btnTwitter.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                Toast.makeText(Contact.this,"kikoo",Toast.LENGTH_LONG).show();
-                shareIt();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://twitter.com/mistraformation")); //catches and opens a url to the desired page
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://plus.google.com/+MistraFr/posts")); //catches and opens a url to the desired page
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnLinkedin.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.linkedin.com/company/2248161")); //catches and opens a url to the desired page
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnViadeo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://fr.viadeo.com/fr/company/mistra")); //catches and opens a url to the desired page
+                startActivity(intent);
+                finish();
             }
         });
     }
 
-    private void shareIt() {
-        //sharing implementation here
-        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
-        sharingIntent.setType("text/plain");
-        String shareBody = "Here is the share content body";
-        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, "Subject Here");
-        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
-        startActivity(Intent.createChooser(sharingIntent, "Partager avec"));
-    }
 
 
     @Override
